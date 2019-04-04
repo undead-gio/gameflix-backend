5a4f4079ea2580c78e9faeba8565b46e9b867a32
manuel mazzatelli <manuel.narut@gmail.com>
2019-04-04 11:31:20 +0200

    selezione categorie

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const fs = require('fs');
 var cors = require('cors');
 var app = express();
 const port = 8080;
-
+var _ = require('lodash');
 let categories = JSON.parse(fs.readFileSync('./data/categories.json'));
 let games = JSON.parse(fs.readFileSync('./data/games.json'));
 
+let categories_id_with_games =_.uniq(_.flatten( _.map(games,'genres')));
+let categories_with_games = _.filter(categories, function(category){
+  return _.includes(categories_id_with_games, category.id);
+})
 app.use(cors())
 
 app.get('/', function (req, res) {
@@ -14,7 +18,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/categories', function (req, res) {
-  res.send(categories);
+  res.send(categories_with_games);
 });
 
 app.get('/games', function (req, res) {
